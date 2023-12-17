def1f7b779145738b00ce82efabf359dc1630c72
MatveyZhizhin <117218670+MatveyZhizhin@users.noreply.github.com>
2023-12-17 22:59:24 +0300

    почистил код

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private float timeAttack;
-    [SerializeField] private float startTimeAttack;
-    [SerializeField] private Transform attackPoint;
-    [SerializeField] private int damage;
-    [SerializeField] private float attackRange;
-    [SerializeField] private LayerMask _whatIsPlayer;
+    private float _timeAttack;
+    [SerializeField] private float _startTimeAttack;
+    [SerializeField] private Transform _attackPoint;
+    [SerializeField] private int _damage;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private LayerMask _layerToIgnore;
 
     private Animator anim;
 
@@ -18,35 +18,35 @@ public class PlayerAttack : MonoBehaviour
 
     private void Attack()
     {
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, ~_whatIsPlayer);
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, ~_layerToIgnore);
         foreach (Collider2D enemy in enemies)
         {
             if (enemy.TryGetComponent(out Health health))
             {
-                health.TakeDamage(damage);
+                health.TakeDamage(_damage);
             }
         }
     }
 
     private void Update()
     {
-        if(timeAttack <= 0)
+        if(_timeAttack <= 0)
         {
             if(Input.GetMouseButton(0))
             {
                 anim.SetTrigger("attack");               
-                timeAttack = startTimeAttack;
+                _timeAttack = _startTimeAttack;
             }
             
         }
         else
         {
-            timeAttack -= Time.deltaTime;
+            _timeAttack -= Time.deltaTime;
         }
     }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        Gizmos.DrawWireSphere(_attackPoint.position, _attackRange);
     }
 }
